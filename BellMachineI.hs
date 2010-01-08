0721e685dd173543d574bb146f4659f47dc42390
maoe <maoe.maoe@gmail.com>
2010-01-08 15:26:01 +0900

    BellMachineI.hs: interactive BellMachine

---
+++ BellMachineI.hs
@@ -0,0 +1,30 @@
+import FRP.Reactive
+import FRP.Reactive.LegacyAdapters
+import Control.Applicative
+import Control.Concurrent
+import Control.Monad
+import Data.Monoid
+import System.IO
+
+type BellMachine = Event () -> Event ()
+
+bell :: BellMachine
+bell = id
+
+beepTimer :: BellMachine
+beepTimer = pure $ atTimes [0, 2..]
+
+nifty :: BellMachine
+nifty = beepTimer `mappend` bell
+
+bellAction :: a -> Action
+bellAction = const $ putStrLn "BEEP!"
+
+main :: IO ()
+main = do
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
+  hSetEcho stdin False
+  (sink, event) <- makeEvent =<< makeClock
+  forkIO $ forever $ getChar >> sink ()
+  adaptE $ bellAction <$> nifty event
