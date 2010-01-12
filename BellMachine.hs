4c66aeeaee00ad03e89385453162671e6cfaeff8
maoe <maoe.maoe@gmail.com>
2010-01-13 00:55:12 +0900

    revert name

--- BellMachine.hs
+++ BellMachine.hs
@@ -2,14 +2,14 @@ import FRP.Reactive                ( Event, atTime, atTimes )
 import FRP.Reactive.LegacyAdapters ( Action, adaptE )
 import Control.Applicative         ( (<$>) )
 
-beepTimer :: Event ()
-beepTimer = atTime 3
+bellTimer :: Event ()
+bellTimer = atTime 3
 
-beepTimer' :: Event ()
-beepTimer' = atTimes [1..]
+bellTimer' :: Event ()
+bellTimer' = atTimes [1..]
 
-beepAction :: () -> Action
-beepAction = const $ putStrLn "BEEP!"
+bellAction :: () -> Action
+bellAction = const $ putStrLn "BEEP!"
 
 main :: IO ()
-main = adaptE $ beepAction <$> beepTimer'
+main = adaptE $ bellAction <$> bellTimer'
