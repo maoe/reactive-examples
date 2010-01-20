06d45ddd12ff98734a2c4d72ebad01fdfdcf08b9
maoe <maoe.maoe@gmail.com>
2010-01-20 14:04:01 +0900

    tweaks

--- Progress.hs
+++ Progress.hs
@@ -1,22 +1,34 @@
 import FRP.Reactive
 import FRP.Reactive.LegacyAdapters
 import Control.Applicative
+import Data.Fixed
+import Text.Printf
+import Data.Monoid
 
-type Progress = Behavior Percentage
+type Progress   = Behavior Percentage
 type Percentage = Double
-type ProgressBar = Behavior Percentage -> Behavior String
+type Width      = Int
 
-runProgressBar :: Behavior Percentage -> Event String
-runProgressBar p = consoleBar p `snapshot_` atTimes [0.1,0.2..]
+renderProgress :: Width -> Progress -> IO ()
+renderProgress w p = adaptE $ render <$> renderBar w percent
+  where percent :: Event Percentage
+        percent = p `snapshot_` atTimes [0.1, 0.2 ..]
 
-progressBar :: Int -> ProgressBar
-progressBar w = fmap bar
+render :: String -> IO ()
+render ss = putChar '\r' >> putStr ss
+
+renderBar :: Width -> Event Percentage -> Event String
+renderBar w = fmap bar
   where bar :: Percentage -> String
-        bar p = replicate (truncate $ (p/100)*fromIntegral w) '#'
+        bar p = replicate (truncate (fromIntegral w*p/100)) '#'
 
-consoleBar :: ProgressBar
-consoleBar = progressBar 80
+renderMeter :: Event Percentage -> Event String
+renderMeter = fmap meter
+  where meter :: Percentage -> String
+        meter p = printf "%3.0f" p ++ " %"
 
-percent = time * 10
+linerProgress :: Progress
+linerProgress = liftA2 mod' (time*10) (pure 100)
 
-main = adaptE $ putStrLn <$> runProgressBar percent
+main :: IO ()
+main = renderProgress 30 linerProgress
